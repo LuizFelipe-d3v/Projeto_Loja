2d8fdb984e1df9f69900b800f740b4932c9d846d
Luiz Felipe <mendes.f@escolar.ifrn.edu.br>
2025-11-18 11:27:05 -0300

    realizando mudancas no HTML e no JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const prevButton = document.getElementById('voltar')
-const nexButton = document.getElementById('avancar')
+const nextButton = document.getElementById('avancar')
 const items = document.querySelectorAll('.item')
 const botoes = document.querySelectorAll('.ponto')
 const numIndicator = document.querySelector('numeros')
@@ -9,6 +9,33 @@ let ativo = 0;
 const total = items.length;
 let timer;
 
-prevButton.addEventListener('click', function() {
-    update()
+function update(direction) {
+
+    document.querySelector('.item.ativo').classList.remove('ativo')
+    document.querySelector('.dot.ativo').classList.remove('ativo')
+
+    if(direction > 0){
+        ativo = ativo + 1
+
+        if(ativo === total){
+            ativo = 0
+        }
+    } 
+    
+    else if(direction < 0){
+
+    }
+
+    items[ativo].classList.add('ativo')
+    botoes[ativo].classList.add('ativo')
+}
+
+
+
+prevButton.addEventListener('click', () => {
+    update(-1)
+})
+
+nextButton.addEventListener('click', () => {
+    update(1)
 })
