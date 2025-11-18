6492d5e288acb3d6d9269b971679cf7fee5238a9
Luiz <mendes.f@escolar.ifrn.edu.br>
2025-11-18 17:17:56 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const prevButton = document.getElementById('voltar')
 const nextButton = document.getElementById('avancar')
 const items = document.querySelectorAll('.item')
 const botoes = document.querySelectorAll('.ponto')
-const numIndicator = document.querySelector('numeros')
+const numIndicator = document.querySelector('.numeros')
 const lista = document.querySelector('lista')
 
 let ativo = 0;
@@ -12,7 +12,7 @@ let timer;
 function update(direction) {
 
     document.querySelector('.item.ativo').classList.remove('ativo')
-    document.querySelector('.dot.ativo').classList.remove('ativo')
+    document.querySelector('.ponto.ativo').classList.remove('ativo')
 
     if(direction > 0){
         ativo = ativo + 1
@@ -23,13 +23,23 @@ function update(direction) {
     } 
     
     else if(direction < 0){
-
+        ativo = ativo - 1
+        
+        if(ativo < 0){
+            ativo = total - 1
+        }
     }
 
     items[ativo].classList.add('ativo')
     botoes[ativo].classList.add('ativo')
+
+    numIndicator.textContent = String(ativo + 1).padStart(2, '0')
 }
 
+clearInterval(timer)
+timer = setInterval(() => {
+        update(1)
+    }, 5000);
 
 
 prevButton.addEventListener('click', () => {
